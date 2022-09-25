832db11511b250d60dfaf82275e7b1b3a362a756
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-25 09:54:07 -0500

    This function seems to be working but have to do more testing

--- arrayAdder.js
+++ arrayAdder.js
@@ -1,9 +1,17 @@
 function arrAdder(arr) {
 	let arrStr = " "
-	for(let i = 0; i < arr.length(); i++) {
+	for(let i = 0; i < arr.length; i++) {
+		arrStr += ' '
 		for(let j = 0; j < arr[i].length; j++) {
 			arrStr += arr[j][i]
+			if (arr[j][i] === "") arrStr += " "
 		}
 	}
-	return arrStr
+	return arrStr.trim()
 }
+
+console.log(arrAdder([
+['J','L','L','M']
+,['u','i','i','a']
+,['s','v','f','n']
+,['t','e','e','']]))
