420dd6e888c8e55f039185f1c141cf9a4c0d493d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-25 09:31:36 -0500

    Had to change from forEach to map in the multiplyAll function in curryingMulti.js script

--- curryingMulti.js
+++ curryingMulti.js
@@ -1,8 +1,7 @@
-alert("Testing")
 function multiplyAll(arr) {
 	return function(number) {
-		return arr.forEach(ele => ele * number)
+		return arr.map(ele => ele * number)
 	}
 }
 
-console.log(multiplyAll([1, 2, 3, 4])(2))
+console.log(multiplyAll([1, 2, 3, 4])(3))
