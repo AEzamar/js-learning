ce1c05b345a7e0771ece5debd82d8a36212c847e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-25 09:38:38 -0500

    Added user submitted solution to curryingMulti.js script

--- curryingMulti.js
+++ curryingMulti.js
@@ -6,3 +6,6 @@ function multiplyAll(arr) {
 
 console.log(multiplyAll([1, 2, 3, 4])(3))
 console.log(multiplyAll([12, 22, 31, 69])(2))
+
+//User submitted solution for the challenge
+//const multiplyAll = arr => n => arr.map(x => n * x);
