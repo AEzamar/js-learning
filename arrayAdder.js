bba7bbed4893ccdb69f05380e518e4068bf88c1e
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-25 09:49:00 -0500

    Trying to solve array adder challenge with javascript in arrayAdder.js script

--- arrayAdder.js
+++ arrayAdder.js
@@ -1,7 +1,9 @@
-alert("New script!")
 function arrAdder(arr) {
 	let arrStr = " "
 	for(let i = 0; i < arr.length(); i++) {
-
+		for(let j = 0; j < arr[i].length; j++) {
+			arrStr += arr[j][i]
+		}
 	}
+	return arrStr
 }
