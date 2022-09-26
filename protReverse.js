bcc69d1f7ee6974c7e2d33a4a31699218461002f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-26 10:17:22 -0500

    Still working on the challenge to make an array reverse method

--- protReverse.js
+++ protReverse.js
@@ -1,11 +1,9 @@
 Array.prototype.reverse = function() {
-	for(let i = 0; i < this.length; i++) {
-		const first = this.shift()
-		console.log(first);
-		this.push(first);
+	for(let i = this.length; i > 0; i--) {
+		this.push(this.shift())
 	}
 	return this;
 };
 
 const numbers = [1, 2, 3, 4]
-console.log(numbers.reverse())
+console.log(numbers.reverse());
