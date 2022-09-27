1c6627a6b77d9c31b67e0f197210ebbd5dee79dd
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-27 07:10:08 -0500

    Corrected some more typos and min function is now working

--- minValue.js
+++ minValue.js
@@ -1,7 +1,8 @@
 function min(arr, toReturn) {
   const minimum = Math.min(...arr)
   if(toReturn.toLowerCase() === 'value') return minimum;
-  else if(toReturn.toLowerCase() === 'index') return arr.indexof(minimum);
+  else if(toReturn.toLowerCase() === 'index') return arr.indexOf(minimum);
 };
 
 console.log(min([2, 4, 8, 1, -3, 0, 5, 7], 'value'));
+console.log(min([2, 4, 8, 1, 3, 0, 5, 7], 'index'));
