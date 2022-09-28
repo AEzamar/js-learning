64f48262ee9a530aca72cc926d4e00bb868ee004
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-28 06:45:47 -0500

    Trying to remember JS Array.from syntax

--- fillArr.js
+++ fillArr.js
@@ -1,5 +1,13 @@
-function fillArr(size) {
-	for(let i = 0; i < size; i++) {
-		
-	}
+const randNum = function() {
+	return Math.floor(Math.random() * 6) + 1;
 }
+
+function fillArr(n) {
+	/*for(let i = 0; i < size; i++) {
+
+	};*/
+
+	return Array.from(length = n, (_, i) => i += randNum());
+};
+
+console.log(fillArr(5));
