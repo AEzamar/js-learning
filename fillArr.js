fc610c589e85665b95442d9b52e6954618b5a8fe
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-28 10:00:03 -0500

    Trying to solve this with an arrow function

--- fillArr.js
+++ fillArr.js
@@ -1,5 +1,6 @@
-function fillArr(n) {
+/*function fillArr(n) {
 	return n > 0 ? [...Array(n).keys()] : [];
-};
+};*/
 
+function arr(N) => N > 0 ? [...Array(N).keys()] : [];
 console.log(fillArr(5));
