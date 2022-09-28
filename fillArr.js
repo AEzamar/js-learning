2d4ac09057ac3b138dba9fc8c213fb58ed3ddd4d
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-28 13:18:59 -0500

    This challenge was solved with an arrow function and submitted

--- fillArr.js
+++ fillArr.js
@@ -2,5 +2,5 @@
 	return n > 0 ? [...Array(n).keys()] : [];
 };*/
 
-const arr = function(N) => N > 0 ? [...Array(N).keys()] : [];
+const arr = (N) => N > 0 ? [...Array(N).keys()] : [];
 console.log(arr(5));
