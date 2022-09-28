7f88728990e08d7e90bee7137ea8a959e48faf62
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-28 07:21:17 -0500

    Cleaned up fillArr.js script

--- fillArr.js
+++ fillArr.js
@@ -3,14 +3,7 @@ const randNum = function() {
 };
 
 function fillArr(n) {
-	/*const newArr = [];
-	for(let i = 0; i < n; i++) {
-		newArr.push(i);
-	};
-	return newArr;*/
 	return Array(n).fill(0).map((_, i) => i)
-	//return Array.from({ length: n}, (_, i) => i = randNum());
-	//return Array.from({ length: n}, (_, i) => i += 0 + n);
 };
 
 console.log(fillArr(5));
