0c7e32c91d4ca5876abbca31d3114f459dbfe6ae
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-29 10:06:28 -0500

    Cleanup the code on maxTriSum.py a bit and added an addtional test case with negative numbers

--- maxTriSum.js
+++ maxTriSum.js
@@ -1,11 +1,11 @@
 function maxTriSum(numbers){
 	const sortedNumbers = numbers.sort((a, b) => b - a);
 	const numbersSet = new Set(sortedNumbers)
-	//console.log(numbersSet)
 	const maxTriplet = Array.from(numbersSet).slice(0, 3);
 	return maxTriplet.reduce((total, curr) => total + curr, 0);
 };
 
-console.log(maxTriSum([3,2,6,8,2,3]))
+console.log(maxTriSum([3,2,6,8,2,3]));
 console.log(maxTriSum([2,9,13,10,5,2,9,5]));
 console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]));
+console.log(maxTriSum([-3,-27,-4,-2,-27,-2]));
