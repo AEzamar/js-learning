73b5705eb0f6f4073e4380147a5e20b6c7f6c3e3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-29 07:05:02 -0500

    Going to test this script in the browser

--- maxTriSum.js
+++ maxTriSum.js
@@ -1,4 +1,7 @@
 function maxTriSum(numbers){
-	const set(sorted_numbers) = numbers.sort((a, b) => b - a);
+	const sortedNumbers = numbers.sort((a, b) => b - a);
+	const maxTriplet = sortedNumbers.slice(0, 3);
+	return maxTriplet.reduce((total, curr) => total + curr, 0);
+};
 
-}
+console.log(maxTriSum([3,2,6,8,2,3]))
