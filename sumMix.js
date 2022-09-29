9bb15f28f0d28de965b5bf742a844cec22cd8417
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-29 06:33:31 -0500

    Changed the name of a variable to make it a little more readable

--- sumMix.js
+++ sumMix.js
@@ -6,7 +6,7 @@ function sumMix(arr) {
 console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
 console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']));*/
 
-const sumMixArrow = (arr) => arr.map(ele => Number(ele)).reduce((total, curr) => total + curr, 0);
+const sumMixArrow = (arr) => arr.map(item => Number(item)).reduce((total, curr) => total + curr, 0);
 
 console.log(sumMixArrow([9, 3, '7', '3']));
 console.log(sumMixArrow(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
