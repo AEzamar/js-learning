0a26fd631a698241eb53ca6a73b8e3987b9ebb40
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-29 06:29:16 -0500

    Trying to solve this challenge with an arrow function

--- sumMix.js
+++ sumMix.js
@@ -2,6 +2,12 @@ function sumMix(arr) {
 	return arr.map(ele => Number(ele)).reduce((total, curr) => total + curr, 0);
 };
 
-console.log(sumMix([9, 3, '7', '3']));
+/*console.log(sumMix([9, 3, '7', '3']));
 console.log(sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
-console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']));
+console.log(sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']));*/
+
+const sumMixArrow = () => arr => arr.map(ele => Number(ele)).reduce((total, curr) => total + curr, 0);
+
+console.log(sumMixArrow([9, 3, '7', '3']));
+console.log(sumMixArrow(['5', '0', 9, 3, 2, 1, '9', 6, 7]));
+console.log(sumMixArrow(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']));
