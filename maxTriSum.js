ecf4bc7e5385be8654a7f54b36df8c9bc3b5f6a2
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-29 07:13:28 -0500

    Trying to make an array of unique values before I reduce the max triplet

--- maxTriSum.js
+++ maxTriSum.js
@@ -1,7 +1,10 @@
 function maxTriSum(numbers){
 	const sortedNumbers = numbers.sort((a, b) => b - a);
-	const maxTriplet = sortedNumbers.slice(0, 3);
+	const numbersSet = new Set(sortedNumbers)
+	const maxTriplet = numbersSet.slice(0, 3);
 	return maxTriplet.reduce((total, curr) => total + curr, 0);
 };
 
 console.log(maxTriSum([3,2,6,8,2,3]))
+console.log(maxTriSum([2,9,13,10,5,2,9,5]));
+console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]));
