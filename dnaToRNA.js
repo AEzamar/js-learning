1d480305d646dd802197615122d1a8714473d645
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-30 07:05:44 -0500

    Had to change name capitalization of function and added a new test case

--- dnaToRNA.js
+++ dnaToRNA.js
@@ -1,7 +1,8 @@
-function dnaToRNA(dna) {
+function DNAtoRNA(dna) {
 	const rna = dna.replace(/T/g, 'U');
 	return dna === "" ? "" : rna; 
-}
+};
 
 console.log(dnaToRNA('GCAT'));
 console.log(dnaToRNA('TTTT'));
+console.log(DNAtoRNA('"GACCGCCGCC"'));
