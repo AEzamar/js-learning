d8a612a5a167703cd013b5ec11564dd17940e945
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-09-30 06:41:20 -0500

    Testing dnaToRNA function

--- dnaToRNA.js
+++ dnaToRNA.js
@@ -1,6 +1,9 @@
-function DNAtoRNA(dna) {
+function dnaToRNA(dna) {
 	for(let char in dna){
-		if(char === "T" ) dna.replace(char, "U")
+		if(char === "T" ) dna.replace(char, "U");
 	}
 	return dna; 
 }
+
+console.log(dnaToRNA('GCAT'));
+console.log(dnaToRNA('TTTT'));
