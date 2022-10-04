ee6afa9133ff4f73deef9e8980e27187c601f61f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-04 13:33:59 -0500

    Trying to debug this script

--- zeroPlentyArr.js
+++ zeroPlentyArr.js
@@ -2,10 +2,10 @@ function zeroPlentiful(arr) {
     if(arr.every(num => num === 0)) return 1;
     if(!arr.includes(0)) return 0;
     let zeroCount = 0, sequenceCount = 0, outerCount = 0;
-    for(let i = 0; i <= arr.length; i++) {
+    for(let i = 0; i < arr.length; i++) {
         if(arr[i] === 0) {
             zeroCount++;
-            if(zeroCount >= 4) {
+            if(zeroCount === 4) {
                 sequenceCount++;
                 outerCount++;
             }
@@ -13,7 +13,8 @@ function zeroPlentiful(arr) {
             zeroCount = 0;
             sequenceCount = 0;
         }
-    return outerCount > 1 ? sequenceCount += outerCount : sequenceCount;
+    console.log('Outer count', outerCount);
+    return outerCount > 0 ? sequenceCount += outerCount : sequenceCount;
     }
 }
 
