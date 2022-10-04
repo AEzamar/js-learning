06f8bef7257083c2ff689f88782198c29e940501
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-04 12:04:58 -0500

    Still going through the zero plentiful challenge

--- zero_plenty_arr.js
+++ zero_plenty_arr.js
@@ -1,4 +1,8 @@
-def zero_plentiful(arr):
-    for(let i = 0; i < arr.length; i++) {
-        
-}
+function zero_plentiful(arr):
+    let zeroCount = 0, sequenceCount = 0, outerCount = 0;
+    for(let i = 0; i <= arr.length; i++) {
+        if(arr[i] === 0) {
+            zeroCount++;
+            if(zeroCount >= 4) sequenceCount++
+        }  
+};
