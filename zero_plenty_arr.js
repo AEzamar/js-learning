02b917dfd372e3ebb7986d6938c6abe5548efca8
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-04 12:17:20 -0500

    Still working on this challenge

--- zero_plenty_arr.js
+++ zero_plenty_arr.js
@@ -1,8 +1,16 @@
 function zero_plentiful(arr):
+    if(arr.every(num => num === 0)) return 1;
+    if(!arr.includes(0)) return 0;
     let zeroCount = 0, sequenceCount = 0, outerCount = 0;
     for(let i = 0; i <= arr.length; i++) {
         if(arr[i] === 0) {
             zeroCount++;
-            if(zeroCount >= 4) sequenceCount++
-        }  
+            if(zeroCount >= 4) {
+                sequenceCount++;
+                outerCount++;
+            }
+        }else {
+            sequenceCount = 0;
+        }
+    return outerCount > 1 ? sequenceCount = outerCount : sequenceCount;
 };
