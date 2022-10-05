91ffb23c3f51a5e17de1be3f8e9cecad94f88e15
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-05 16:21:45 -0500

    Trying to solve sortValueIndex.js challenge

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,6 +1,7 @@
 function sortByValueAndIndex(arr)
 {
-	return arr.map((ele, i, arr) => arr.sort((a, b) => (a * (i + 1)) - (b + (i +1))))
-}
+	//return arr.map((ele, i, arr) => arr.sort((a, b) => (a * (i + 1)) - (b + (i +1))))
+	return arr.map((ele, i) => ele  * i).sort((a, b) => a - b);
+} 
 
 console.log(sortByValueAndIndex([23, 2, 3, 4, 5]))
