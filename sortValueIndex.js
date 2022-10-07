44746e722a82453c5cbf6fe000d79ae352def7ee
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-07 13:21:17 -0500

    Still trying to solve the challenge in sortValueIndex.js I got the function to sort but I'm not getting the result I want

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,8 +1,8 @@
 function sortByValueAndIndex(arr)
 {
 	//return arr.map((ele, i, arr) => arr.sort((a, b) => (a * (i + 1)) - (b + (i +1))))
-	let sortedArr = arr.map((_, i, ary) => ary.sort((a, b) => (a * i + 1) - (b * i + 1)));
-	return sortedArr;
+	let sortedArr = arr.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	return  Array(...new Set(...sortedArr));
 } 
 
 console.log(sortByValueAndIndex([23, 2, 3, 4, 5]));
