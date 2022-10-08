22b608e8a29cdb435c62abc9540aeb76c32894ef
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-08 09:26:32 -0500

    Changed named of parameter in sortValueIndex.js

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,7 +1,7 @@
-function sortByValueAndIndex(arr)
+function sortByValueAndIndex(array)
 {
 	//return arr.map((ele, i, arr) => arr.sort((a, b) => (a * (i + 1)) - (b + (i +1))))
-	let sortedArr = arr.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	return  Array(...new Set(...sortedArr));
 } 
 
