c70d08feffb1d1938562e850a37f6cc8297aa610
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-11 06:22:02 -0500

    I can't find the way to solve this challenge only with a custom sort method

--- minMaxArray.js
+++ minMaxArray.js
@@ -1,13 +1,12 @@
 function solve(arr) {
-	return arr.sort((a, b) => Math.max(a) - Math.min(a));
-	/*let minMaxArr = [];
+	let minMaxArr = [];
 	const sortedArr = arr.sort((a, b) => b - a)
 	while(sortedArr.length) {
 		minMaxArr.push(Math.max(...sortedArr), Math.min(...sortedArr));
 		sortedArr.shift();
 		sortedArr.pop();
 	}
-	return Array.from(new Set(minMaxArr));*/
+	return Array.from(new Set(minMaxArr));
 }
 
 console.log(solve([15,11,10,7,12]));
