bf6521b32e70dccc432d1e3e7bf77c0f09ea4235
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-11 06:18:51 -0500

    Trying to refactor minMaxArray.js, the function works and passed the tests but fells a bit sloppy

--- minMaxArray.js
+++ minMaxArray.js
@@ -1,12 +1,13 @@
 function solve(arr) {
-	let minMaxArr = [];
+	return arr.sort((a, b) => Math.max(a) - Math.min(a));
+	/*let minMaxArr = [];
 	const sortedArr = arr.sort((a, b) => b - a)
 	while(sortedArr.length) {
 		minMaxArr.push(Math.max(...sortedArr), Math.min(...sortedArr));
 		sortedArr.shift();
 		sortedArr.pop();
 	}
-	return Array.from(new Set(minMaxArr));
+	return Array.from(new Set(minMaxArr));*/
 }
 
 console.log(solve([15,11,10,7,12]));
