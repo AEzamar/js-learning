788c44c8d9cb6551b2ecd86080c73c52a002745a
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 12:56:16 -0500

    The function is not working, it only returns the last element of the input copy array

--- arrayParity.js
+++ arrayParity.js
@@ -1,10 +1,11 @@
 function solve(arr){
     const copyArr = [...arr];
-    let noParity = 0;
+    let noParity;
     let parityIndex = 0;
     for(let i = 0; i < copyArr.length; i++) {
-        if(copyArr[parityIndex] > 0 && copyArr[parityIndex] !== -Math.abs(copyArr[i])) noParity = copyArr[parityIndex];
-        else if(copyArr[parityIndex] < 0 && copyArr[parityIndex] !== Math.abs(copyArr[i])) noParity = copyArr[parityIndex];
+        noParity = 0;
+        if(copyArr[i] > 0 && copyArr[i] !== -Math.abs(copyArr[i])) noParity = copyArr[i];
+        else if(copyArr[i] < 0 && copyArr[i] !== Math.abs(copyArr[i])) noParity = copyArr[i];
         //console.log(copyArr[parityIndex])
         /*if(copyArr[parityIndex] > 0) {
             if copyArr[i] !== -Math.abs(copyArr[parityIndex]) noParity = copyArr[parityIndex];
@@ -16,4 +17,6 @@ function solve(arr){
     return noParity;
 };
 
-console.log(solve([1, -1, 2, -2, 3]))
+console.log(solve([1, -1, 2, -2, 3]));
+console.log(solve([-3, 1, 2, 3, -1, -4, -2]));
+console.log(solve([1, -1, 2, -2, 3, 3]));
