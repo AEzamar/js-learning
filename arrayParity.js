8c608c243adbbb60e8f863e5ab5c59a104523680
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 06:39:26 -0500

    Solving the element parity in JS

--- arrayParity.js
+++ arrayParity.js
@@ -1,3 +1,8 @@
 function solve(arr){
-    
+    const copyArr = [...arr];
+    let noParity = 0
+    for(let i = 0; i < copyArr.length; i++) {
+        let first = copyArr.shift();
+        copyArr[i] === -first
+    }
 };
