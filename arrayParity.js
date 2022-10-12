3f6e150e426fc0191ce9a2a84c57bd3cebc1819b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 12:44:01 -0500

    The challenge was submitted but it fails two of the 5 initial tests

--- arrayParity.js
+++ arrayParity.js
@@ -1,12 +1,10 @@
 function solve(arr){
     const copyArr = [...arr];
-    console.log(copyArr);
     let noParity = 0;
     let parityIndex = 0;
     for(let i = 0; i < copyArr.length; i++) {
-        for(let j = 0; j < copyArr.length; j++)
-            if(copyArr[parityIndex] > 0 && copyArr[parityIndex] !== -Math.abs(copyArr[i])) noParity = copyArr[parityIndex];
-            else if(copyArr[parityIndex] < 0 && copyArr[parityIndex] !== Math.abs(copyArr[i])) noParity = copyArr[parityIndex];
+        if(copyArr[parityIndex] > 0 && copyArr[parityIndex] !== -Math.abs(copyArr[i])) noParity = copyArr[parityIndex];
+        else if(copyArr[parityIndex] < 0 && copyArr[parityIndex] !== Math.abs(copyArr[i])) noParity = copyArr[parityIndex];
         //console.log(copyArr[parityIndex])
         /*if(copyArr[parityIndex] > 0) {
             if copyArr[i] !== -Math.abs(copyArr[parityIndex]) noParity = copyArr[parityIndex];
