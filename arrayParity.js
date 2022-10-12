2d04c7eed3a6eb44379789454aa03b139474b759
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-12 06:57:48 -0500

    I'm popping the first element in the array inside the for loop at the end of the loop there's not going to be anything to compare the last number to

--- arrayParity.js
+++ arrayParity.js
@@ -1,7 +1,14 @@
 function solve(arr){
     const copyArr = [...arr];
-    let noParity = 0
+    let noParity = 0;
     for(let i = 0; i < copyArr.length; i++) {
         let first = copyArr.shift();
-    }
+        if(first > 0) {
+            if copyArr !== -Math.abs(first) noParity = first;
+        }else if(first < 0) {
+            if copyArr !== Math.abs(first) noParity = first;
+        };
+    };
 };
+
+console.log(solve([1, -1, 2, -2, 3]))
