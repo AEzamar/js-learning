60bab34b78ac29e6522db590c79750c6c5f138e3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-13 06:15:38 -0500

    Still working on arrayParity.js

--- arrayParity.js
+++ arrayParity.js
@@ -1,18 +1,11 @@
 function solve(arr){
     const copyArr = [...arr];
-    let noParity;
+    let noParity = 0;
     let parityIndex = 0;
     for(let i = 0; i < copyArr.length; i++) {
-        noParity = 0;
-        if(copyArr[i] > 0 && copyArr[i] !== -Math.abs(copyArr[i])) noParity = copyArr[i];
-        else if(copyArr[i] < 0 && copyArr[i] !== Math.abs(copyArr[i])) noParity = copyArr[i];
-        //console.log(copyArr[parityIndex])
-        /*if(copyArr[parityIndex] > 0) {
-            if copyArr[i] !== -Math.abs(copyArr[parityIndex]) noParity = copyArr[parityIndex];
-        }else if(copyArr[parityIndex] < 0) {
-            if copyArr[i] !== Math.abs(copyArr[parityIndex]) noParity = copyArr[parityIndex];
-        }*/
-        parityIndex++;
+        copyArr.forEach(function(_, j) {
+            if(copyArr[j] > 0 && copyArr[j] !== -Math.abs(copyArr[i])) noParity = copyArr[j];
+        });
     };
     return noParity;
 };
