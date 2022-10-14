e8dcbfae1c7a22b9305a833e6f1c657e6b321d26
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-14 06:45:00 -0500

    This function is getting more complicated, I still need to see if it works

--- arrayParity.js
+++ arrayParity.js
@@ -4,8 +4,9 @@ function solve(arr) {
     let noParity;
     const positiveArr = copyArr.filter(item => item > 0);
     const negativeArr = copyArr.filter(item => item < 0);
+    const dynamicLen = positiveArr.length > negativeArr.length || negativeArr.length > positiveArr.length;
     for(let i = 0; i < copyArr.length; i++) {
-
+        positiveArr[i] !== negativeArr
     }
     //let noParity = copyArr.filter(item => item !== -Math.abs(item));
     //let noParity = !copyArr.includes(item => item !== -Math.abs(item) || item !== Math.abs(item));    
