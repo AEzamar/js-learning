05ac942df642151cce5ceea5109d153dfa97be86
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-14 07:05:32 -0500

    The conditional inside the for loop, made with a ternary operator doesn't seems to be working right, going to try a regular else if bloc

--- arrayParity.js
+++ arrayParity.js
@@ -2,12 +2,12 @@ function solve(arr) {
     const copyArr = [...arr];
     //let i = 0
     let noParity;
-    const positiveArr = copyArr.filter(item => item > 0);
-    const negativeArr = copyArr.filter(item => item < 0);
+    const positiveArr = copyArr.filter(item => item > 0).sort((a, b) => a - b);
+    const negativeArr = copyArr.filter(item => item < 0).sort((a, b) => a - b);
     const dynamicLen = positiveArr.length > negativeArr.length ? positiveArr.length : negativeArr.length;
     //console.log(dynamicLen);
     for(let i = 0; i < dynamicLen; i++) {
-        positiveArr[i] !== negativeArr[i] ? noParity = positiveArr[i] : noParity = negativeArr[i];
+        //positiveArr[i] !== negativeArr[i] ? noParity = positiveArr[i] : noParity = negativeArr[i];
     }
     //let noParity = copyArr.filter(item => item !== -Math.abs(item));
     //let noParity = !copyArr.includes(item => item !== -Math.abs(item) || item !== Math.abs(item));    
