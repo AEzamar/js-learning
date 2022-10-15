51e76cf376a3fd50b65c4b6b63e309c402d6422c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-15 09:58:03 -0500

    I already tried to separate the posititve and negative numbers into separate arrays and compare them and return the one that has no negative or positive counterpart

--- arrayParity.js
+++ arrayParity.js
@@ -1,14 +1,14 @@
 function solve(arr) {
     const copyArr = [...arr];
-    let noParity;
+    let noParity = 0;
     const positiveArr = copyArr.filter(item => item > 0).sort((a, b) => a - b);
     const negativeArr = copyArr.filter(item => item < 0).sort((a, b) => a - b);
     const dynamicLen = positiveArr.length > negativeArr.length ? positiveArr.length : negativeArr.length;
-    for(let i = 0; i < dynamicLen; i++) {
+    for(let i = 0; i < arr; i++) {
         if(positiveArr[i] !== negativeArr[i]) {
-            noParity = positiveArr[i];
+            noParity += positiveArr[i];
         }else if(negativeArr[i] !== positiveArr[i]){
-            noParity = negativeArr[i];
+            noParity += negativeArr[i];
         }
         //positiveArr[i] !== negativeArr[i] ? noParity = positiveArr[i] : noParity = negativeArr[i];
     }
@@ -17,22 +17,7 @@ function solve(arr) {
     return noParity;
 };
 
-/*console.log(solve([1, -1, 2, -2, 3]));
+console.log(solve([1, -1, 2, -2, 3]));
 console.log(solve([1, 3, 2, -1, -3]));
 console.log(solve([-3, 1, 2, 3, -1, -4, -2]));
-console.log(solve([1, -1, 2, -2, 3, 3]));*/
-
-function solve1(arr) {
-    return arr.filter(function(_, i, arr1) {
-        if(arr1[i] > 0) {
-            return arr1[i] !== -Math.abs(arr1[i]);
-        }else if(arr1[i] < 0) {
-            return arr1[i] !== Math.abs(arr1[i]);
-        }
-    });
-}
-
-console.log(solve1([1, -1, 2, -2, 3]));
-console.log(solve1([1, 3, 2, -1, -3]));
-console.log(solve1([-3, 1, 2, 3, -1, -4, -2]));
-console.log(solve1([1, -1, 2, -2, 3, 3]));
+console.log(solve([1, -1, 2, -2, 3, 3]));
