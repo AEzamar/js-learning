9440c8c5a5d18421654ad8f7e818f5fb8eed0218
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 11:43:33 -0500

    The function is just returning the array in ascending order

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -2,10 +2,12 @@ function sortByValueAndIndex(array)
 {
 	//return arr.map((ele, i, arr) => arr.sort((a, b) => (a * (i + 1)) - (b + (i +1))))
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	let sortedArr;
 	for(let i = 0; i < array.length; i++) {
-		array.sort((a, b) => a * i - b * i);
+		sortedArr = array.sort((a, b) => a * (i + 1) - b * (i + 1));
 	}
-	return  Array(...new Set(...sortedArr));
+	return sortedArr;
+	//return  Array(...new Set(...sortedArr));
 } 
 
 console.log(sortByValueAndIndex([23, 2, 3, 4, 5]));
