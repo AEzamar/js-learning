9084afeefdd633c7d66413362446700248f72f9b
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 13:29:08 -0500

    Deleted something I should not have in the map and sort line

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,7 +1,7 @@
 function sortByValueAndIndex(array)
 {
-	//let sortedArr = array.map((_, i) => ary.sort((a, b) => a * i + 1 - b * i + 1));
-	let sortedArr;
+	let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	//let sortedArr;
 	for(let i = 0; i < array.length; i++) {
 		sortedArr = array.sort((a = a * i + 1, b = b * i + 1) => a - b);
 	}
