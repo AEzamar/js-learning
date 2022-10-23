beb585bcea79c134e6e6c0eef2d08d68cb3763b9
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 14:56:39 -0500

    Using syntax taken from MDN documentation about sort method

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -3,7 +3,12 @@ function sortByValueAndIndex(array)
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	//let sortedArr;
 	for(let i = 0; i < array.length; i++) {
-		sortedArr = array.sort((a = a * (i + 1), b = b * (i + 1)) => a - b);
+		sortedArr = array.sort((a, b)=> {
+			const productA = a * i + 1;
+			const productB = b * i + 1; 
+			if(productA < productB) return -1;
+			if(productB > productB) return 1;
+		});
 	}
 	return sortedArr;
 	//return  Array(...new Set(...sortedArr));
