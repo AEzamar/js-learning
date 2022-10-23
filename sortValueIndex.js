384a0add88084d427b48e50d6b8119c39f8cd6f3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 11:35:28 -0500

    Trying some new syntax on sortValueIndex.js

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,7 +1,10 @@
 function sortByValueAndIndex(array)
 {
 	//return arr.map((ele, i, arr) => arr.sort((a, b) => (a * (i + 1)) - (b + (i +1))))
-	let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	for(let i = 0; i < array.length; i++) {
+		array.sort((a, b) => a * i - b * i);
+	}
 	return  Array(...new Set(...sortedArr));
 } 
 
