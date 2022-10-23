56156cda2c6d8152100d23ea444fcb97a9c82988
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 15:14:29 -0500

    Added some extra elements to the syntax to see if that works

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -3,10 +3,14 @@ function sortByValueAndIndex(array)
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	//let sortedArr;
 		let sortedArr = array.map((_, i, arr) => arr.sort((a, b)=> {
-			const productA = a * (i + 1);
-			const productB = b * (i + 1); 
-			if(productA < productB) return -1;
-			if(productB > productB) return 1;
+			const productA = a * i + 1;
+			const productB = b * i + 1; 
+			if(productA < productB) {
+				return -1;
+			}
+			if(productB > productB) {
+				return 1;
+			}
 			return 0;
 			}));
 		return sortedArr;
