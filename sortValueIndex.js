455485733275a56168cca129b098814859290968
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 14:21:52 -0500

    Have to make this sort work

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -3,7 +3,7 @@ function sortByValueAndIndex(array)
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	//let sortedArr;
 	for(let i = 0; i < array.length; i++) {
-		sortedArr = array.sort((a = a * i + 1, b = b * i + 1) => a - b);
+		sortedArr = array.sort((a = a * (i + 1), b = b * (i + 1)) => a - b);
 	}
 	return sortedArr;
 	//return  Array(...new Set(...sortedArr));
