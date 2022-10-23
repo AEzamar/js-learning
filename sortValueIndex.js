590b454ad8d19a6cffe326844b61d33082517f50
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 12:16:29 -0500

    I'm trying to get the right output in the output array in sortValueIndex.js

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -4,7 +4,7 @@ function sortByValueAndIndex(array)
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	let sortedArr;
 	for(let i = 0; i < array.length; i++) {
-		sortedArr = array.sort((a, b) => a * (i + 1) - b * (i + 1));
+		sortedArr = array.sort((a, b) => (a * i + 1) - (b * i + 1));
 	}
 	return sortedArr;
 	//return  Array(...new Set(...sortedArr));
