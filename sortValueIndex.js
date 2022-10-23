2acae177723df1ce239a1d141bd3a8d5cef5095f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 15:10:33 -0500

    Experimenting with map and sort and I'm getting the same nested arrays wwith the array sorted in ascending order

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -2,17 +2,15 @@ function sortByValueAndIndex(array)
 {
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	//let sortedArr;
-	for(let i = 0; i < array.length; i++) {
-		sortedArr = array.sort((a, b)=> {
+		let sortedArr = array.map((_, i, arr) => arr.sort((a, b)=> {
 			const productA = a * (i + 1);
 			const productB = b * (i + 1); 
 			if(productA < productB) return -1;
 			if(productB > productB) return 1;
 			return 0;
-		});
-	}
-	return sortedArr;
+			}));
+		return sortedArr;
 	//return  Array(...new Set(...sortedArr));
-} 
+};
 
 console.log(sortByValueAndIndex([23, 2, 3, 4, 5]));
