b341ccd2c940c1419abf7b60a57178cd9fa4f262
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-23 14:16:28 -0500

    I have to check in MDN if there's a way to make more complex sort functions the way I want to

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,6 +1,6 @@
 function sortByValueAndIndex(array)
 {
-	let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
+	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	//let sortedArr;
 	for(let i = 0; i < array.length; i++) {
 		sortedArr = array.sort((a = a * i + 1, b = b * i + 1) => a - b);
