077b9089217dae3db37f62df073d3d516d5d5464
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-25 07:09:37 -0500

    The result is correct

--- sumArrAverage.js
+++ sumArrAverage.js
@@ -1,12 +1,12 @@
 const sumAverage = (arr) => {
   let result = 0;
   const average = (subArr) => {
-    return subArr.reduce((total, curr, _, arr) => (total + curr) / arr.length, 0);
+    return subArr.reduce((total, curr, arr) => total + curr / arr.length, 0);
   }
   for(let i = 0; i < arr.length; i++) {
     result += average(arr[i]);
   }
-  return result;
+  return Math.floor(result);
 }
 
 console.log(sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]]));
