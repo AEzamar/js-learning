f03aa634011418a2ee4a451c2d4f78c9855938df
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-25 07:05:32 -0500

    Solving sumArrAverage.js challenge with a small helper function that calculates the average of subrrays and then use the helper function in a fort loop to apply the functio to every sub array

--- sumArrAverage.js
+++ sumArrAverage.js
@@ -1,5 +1,10 @@
 const sumAverage = (arr) => {
-  let result;
-  // Your code here
+  let result = 0;
+  const average = (subArr) => {
+    return subArr.reduce((total, curr, _, arr) => (total + curr) / arr.length, 0);
+  }
+  for(let i = 0; i < arr.length; i++) {
+    result += average(arr[i]);
+  }
   return result;
 }
