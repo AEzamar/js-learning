feddc7b4df01450fd4567e0844550115921bb1bc
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-25 06:57:32 -0500

    Deleted some unused code in sortValueIndex.js

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -1,7 +1,5 @@
 function sortByValueAndIndex(array)
 {
-	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
-	//let sortedArr;
 		let sortedArr = array.sort((a, b) => {
 			let indexer = 1
 			const productA = a * indexer;
@@ -16,7 +14,6 @@ function sortByValueAndIndex(array)
 			return 0;
 			});
 		return sortedArr;
-	//return  Array(...new Set(...sortedArr));
 };
 
 console.log(sortByValueAndIndex([23, 2, 3, 4, 5]));
