8a4ff2f8b78ed89b55b5a26a2187ed682f1de95f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-25 06:33:52 -0500

    That custom sort is not working in sortValueIndex.js

--- sortValueIndex.js
+++ sortValueIndex.js
@@ -2,7 +2,7 @@ function sortByValueAndIndex(array)
 {
 	//let sortedArr = array.map((_, i, ary) => ary.sort((a, b) => a * i + 1 - b * i + 1));
 	//let sortedArr;
-		let sortedArr = array.map((_, i, arr) => arr.sort((a, b)=> {
+		let sortedArr = array.sort((a, b)=> {
 			const productA = a * i + 1;
 			const productB = b * i + 1; 
 			if(productA < productB) {
@@ -12,7 +12,7 @@ function sortByValueAndIndex(array)
 				return 1;
 			}
 			return 0;
-			}));
+			});
 		return sortedArr;
 	//return  Array(...new Set(...sortedArr));
 };
@@ -21,8 +21,16 @@ console.log(sortByValueAndIndex([23, 2, 3, 4, 5]));
 
 function sortByValueAndIndex1(array)
 {
+	let sortedArray;
 	function customSort(a, b, index) {
-		if(a * index + 1 < b * index + 1) return -1
-		if(b * index + 1 >)
-	}
+		if(a * index + 1 < b * index + 1) return -1;
+		if(b * index + 1 > a * index + 1) return 1;
+		return 0;
+	};
+	for(let i = 0; i < array.length; i++) {
+		sortedArray = array.sort(customSort)
+	};
+	return sortedArray;
 }
+
+console.log(sortByValueAndIndex1([23, 2, 3, 4, 5]));
