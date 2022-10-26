f148097a9e3ffbcb1ca791fd29c9938a99d956ab
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-25 21:09:18 -0500

    Just a little mistake with the result variable

--- sumArrAverage.js
+++ sumArrAverage.js
@@ -1,7 +1,7 @@
 const sumAverage = (arr) => {
-  let result;
+  let result = 0;
   for(let i = 0; i < arr.length; i++) {
-    result += arr[i].reduce((total, curr, _, arr1) => total + curr / arr1.length, 0);
+    result += arr[i].reduce((total, curr, _, arr) => total + curr / arr.length, 0);
   }
   return Math.floor(result);
 }
