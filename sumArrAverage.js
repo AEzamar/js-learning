b438ed920c3f8dcc6756e071004fcddb436c6b51
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-25 20:57:28 -0500

    Going back to the for loop syntax in sumArrAverage.js

--- sumArrAverage.js
+++ sumArrAverage.js
@@ -1,6 +1,9 @@
 const sumAverage = (arr) => {
-  return arr.map(subArr => subArr.reduce((total, curr, _, arr) => total + curr / arr.length, 0));
-  //return Math.floor(result);
+  let result;
+  for(let i = 0; i < arr.length; i++) {
+    result += arr[i].reduce((total, curr, _, arr1) => total + curr / arr1.length, 0);
+  }
+  return Math.floor(result);
 }
 
 console.log(sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]]));
