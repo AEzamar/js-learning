393a2f3e6b9997044f1c299ae7f3d432d3aaa3ac
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-28 06:59:57 -0500

    Had to make copy of array to prevent function from modifying input array

--- loopArr.js
+++ loopArr.js
@@ -2,7 +2,8 @@
 //removing elements from start and adding them to end of array one-by-one (if direction is "left")
 function loopArr(arr, direction, steps) {
 	const outputArr = [];
-	const rightLoop = arr.splice(0,)
+	const copyArr = [...arr]
+	console.log(copyArr.splice(steps, steps + 1, 1));
 	if(direction.toLowerCase() === 'left') {
 		for(let i = 0; i < arr.length; i++) {
 			outputArr.push(arr.splice(steps, steps + 1, 1));
