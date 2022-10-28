9ad636b02b0fc8e0b2a40845ddfff892a2027b1c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-28 06:48:12 -0500

    The challenge i'm solving is a little more complicated since I need to loop trought an arrray and come bac around it depending on the number of steps passed as an argument

--- loopArr.js
+++ loopArr.js
@@ -1,3 +1,9 @@
+//removing from end and adding them to start one by-one (if direction is "right")
+//removing elements from start and adding them to end of array one-by-one (if direction is "left")
 function loopArr(arr, direction, steps) {
-	const rightLoop = 
+	const outputArr = [];
+	const rightLoop = arr.splice(0,)
+	if(direction.toLowerCase === 'left') {
+
+	}
 }
