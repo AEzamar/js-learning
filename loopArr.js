4fa9b4ff9b919d8736bd8e1c33133bab767f9bea
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-29 10:48:03 -0500

    Just somee very minor syntax changes and this is not working, it fails almost half of the random tests

--- loopArr.js
+++ loopArr.js
@@ -10,7 +10,7 @@ function loopArr(arr, direction, steps) {
 		if(copyArr.length) outputArr.push(...copyArr.slice(0, copyArr.length));
 	}else if(direction.toLowerCase() === "right") {
 		for(let j = copyArr.length; j >= 0; j--) {
-			outputArr.unshift(...copyArr.splice(copyArr.length - 1 - steps, 1));
+			outputArr.unshift(...copyArr.splice((copyArr.length - 1) - steps, 1));
 		};
 	};
 	return outputArr;
