05442386d5f0034ecb67a0c4bf632684be76393c
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-29 10:35:45 -0500

    Added a few missing semi-colons

--- loopArr.js
+++ loopArr.js
@@ -6,15 +6,15 @@ function loopArr(arr, direction, steps) {
 	if(direction.toLowerCase() === 'left') {
 		for(let i = 0; i <= copyArr.length; i++) {
 			outputArr.push(...copyArr.splice(steps, 1));
-		}
+		};
 		if(copyArr.length) outputArr.push(...copyArr.slice(0, copyArr.length));
 	}else if(direction.toLowerCase() === "right") {
 		for(let j = copyArr.length; j >= 0; j--) {
 			outputArr.unshift(...copyArr.splice(copyArr.length - 1 - steps, 1));
-		}
-	}
+		};
+	};
 	return outputArr;
-}
+};
 
 console.log(loopArr([1, 5, 87, 45, 8, 8], "left", 2));
 console.log(loopArr([1, 5, 87, 45, 8, 8], "right", 2));
