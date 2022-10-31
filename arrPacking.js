d008710189f140d556c29bb092cafd8d3c2b14b7
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-31 09:40:28 -0600

    I feel like I'm getting close to solving this challenge

--- arrPacking.js
+++ arrPacking.js
@@ -1,10 +1,10 @@
 function arrayPacking(a) {
-	return a
+	return parseInt(a
 		.map(num => num.toString(2))
 		.sort((a, b) => a - b)
 		.map(binStr => binStr.padStart(8, '0'))
-		//.map(binNum => )
-		//.reduce((total, curr) => total + curr , 0)
+		.reduce((total, curr) => total + curr , 0)
+		, 2)
 		//.map(binNum => parseInt(binNum, 2))
 		//.sort((a, b) => a - b)
 }
