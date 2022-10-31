189f0dad970d4950118b75f69fdbd029f81734c5
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-31 11:47:19 -0600

    Going to use join instead of reduce on the binary numbers

--- arrPacking.js
+++ arrPacking.js
@@ -3,7 +3,7 @@ function arrayPacking(a) {
 		.map(num => num.toString(2))
 		.sort((a, b) => a - b)
 		.map(binStr => binStr.padStart(8, '0'))
-		.reduce((total, curr) => total + curr , 0)
+		//.reduce((total, curr) => total + curr , 0)
 		, 2)
 		//.map(binNum => parseInt(binNum, 2))
 		//.sort((a, b) => a - b)
