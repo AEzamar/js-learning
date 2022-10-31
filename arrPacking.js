06ec0cf30908517a629775cde9d0b750147325ca
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-31 07:00:19 -0600

    Working my way through this

--- arrPacking.js
+++ arrPacking.js
@@ -1,5 +1,5 @@
 function arrayPacking(a) {
-	return a.map(num => toString(num, 2)).sort((a, b) => a - b)
+	return a.map(num => num.toString(2)).sort((a, b) => a - b).map(binNum => Number(binNum)).reduce((total, curr) => total + curr, 0);
 }
 
 console.log(arrayPacking([24, 85, 0]));
