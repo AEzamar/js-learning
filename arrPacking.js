970a86467998b32598afb9f60db15b81e2afbad3
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-10-31 22:08:14 -0600

    Still getting the same weird result as before

--- arrPacking.js
+++ arrPacking.js
@@ -3,12 +3,13 @@ function arrayPacking(a) {
 		.map(num => num.toString(2))
 		.sort((a, b) => a - b)
 		.map(binStr => binStr.padStart(8, '0'))
-		//.reduce((total, curr) => total + curr , 0)
+		.join("")
 		, 2)
+		//.reduce((total, curr) => total + curr , 0)
 		//.map(binNum => parseInt(binNum, 2))
 		//.sort((a, b) => a - b)
 }
 
 console.log(arrayPacking([24, 85, 0]));
-console.log(parseInt('1111', 2));
-console.log(parseInt('11000', 2));
+//console.log(parseInt('1111', 2));
+//console.log(parseInt('11000', 2));
