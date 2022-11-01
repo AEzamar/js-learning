1c143521fa7e96349b2886e8a5f497ca67fe5a17
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-01 06:59:00 -0600

    Working my way through simpleArrProduct.js

--- simpleArrProduct.js
+++ simpleArrProduct.js
@@ -1,7 +1,9 @@
-function solve(arr){
+function solve(arr) {
+    let maxProduct = 0;
     for(let i = 0; i < arr.length; i++) {
-
+        maxProduct += arr[i].reduce((total, curr) total * curr, 0);
     }
+    return maxProduct;
 }
 
 console.log(solve())
