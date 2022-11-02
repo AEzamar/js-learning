411cd1e649831e07d309ad1470fe28d8b8562705
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-02 07:42:25 -0600

    I want to make this so I can grab the largest multiplication from all the numbers but I'm not sure how

--- simpleArrProduct.js
+++ simpleArrProduct.js
@@ -6,7 +6,7 @@ function multiplyArr(arr) {
 
 function solve(arr) {
     let maxProduct = 0;
-    const [...spreadArr] = [...arr];
+    const spreadArr = arr.reduce((total, curr) => total.concat(curr));
     console.log(arr.reduce((total, curr) => total + curr));
     console.log(spreadArr);
     return arr.map(subArr => subArr.reduce((total, curr) => total * curr, 0));
