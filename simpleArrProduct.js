ff2bbde5d0ecf303fc9a9945d1793e5b7cb667d2
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-02 07:24:10 -0600

    Trying to flatten a 2d array

--- simpleArrProduct.js
+++ simpleArrProduct.js
@@ -1,7 +1,15 @@
+function multiplyArr(arr) {
+    for(let i = 0; i < arr.length; i++) {
+
+    }
+}
+
 function solve(arr) {
     let maxProduct = 0;
+    const spreadArr = [...arr];
+    console.log(spreadArr);
     return arr.map(subArr => subArr.reduce((total, curr) => total * curr, 0));
     //return maxProduct;
-}
+};
 
 console.log(solve([[1, 2],[3, 4]]));
