3d1d4b6856517b4a77c5e1c3ba40f56fb439650f
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-02 07:50:08 -0600

    Just trying to dynamically multiply numbers in the array

--- simpleArrProduct.js
+++ simpleArrProduct.js
@@ -5,9 +5,11 @@ function multiplyArr(arr) {
 }
 
 function solve(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        console.log(arr[i][i] * arr[i + 1][i])
+    }
     let maxProduct = 0;
     const spreadArr = arr.reduce((total, curr) => total.concat(curr));
-    console.log(arr.reduce((total, curr) => total + curr));
     console.log(spreadArr);
     return arr.map(subArr => subArr.reduce((total, curr) => total * curr, 0));
     //return maxProduct;
