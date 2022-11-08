f6cf6b8bae7f52beb9175647d498025792397b93
Eduardo Azamar <eduardoazamarb@hotmail.com>
2022-11-07 19:38:46 -0600

    Getting an unexpected result on this one

--- simpleArrProduct.js
+++ simpleArrProduct.js
@@ -1,6 +1,6 @@
 function multiplyArr(arr) {
     let i = 0;
-    let maxProduct = 0;
+    let maxProduct = 1;
     while(arr[i].length) {
         maxProduct += arr[i].shift() * arr[i].pop();
         i++;
@@ -11,7 +11,7 @@ function multiplyArr(arr) {
 function solve(arr) {
     let maxProduct = 0;
     for(let i = 0; i < arr.length; i++) {
-        maxProduct = multiplyArr(arr[i]);
+        maxProduct += multiplyArr(arr[i]);
         //console.log(arr[i][i] * arr[i + 1][i])
     }
     /*const spreadArr = arr.reduce((total, curr) => total.concat(curr));
